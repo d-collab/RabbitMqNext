09528aaf021f4299bd2e47f48facfc4cf86df410
hammett <hammett@gmail.com>
2016-01-09 05:13:13 -0800

    experimenting with a lock impl using CAS and delaying the actual waiting (spin)

---
+++ src/RabbitMqNext.Tests/AutoResetSuperSlimLockTestCase.cs
@@ -0,0 +1,238 @@
+namespace RabbitMqNext.Tests
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Threading;
+	using System.Threading.Tasks;
+	using FluentAssertions;
+	using Internals.RingBuffer.Locks;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class AutoResetSuperSlimLockTestCase
+	{
+		[Test]
+		public void InternalOps()
+		{
+			var autoResSlim1 = new AutoResetSuperSlimLock(false);
+
+			autoResSlim1.IsSet.Should().BeFalse();
+			autoResSlim1.Waiters.Should().Be(0);
+			autoResSlim1.ToRelease.Should().Be(0);
+
+			autoResSlim1.Waiters = 34;
+			autoResSlim1.Waiters.Should().Be(34);
+			autoResSlim1.ToRelease.Should().Be(0);
+
+			autoResSlim1.ToRelease = 17;
+			autoResSlim1.ToRelease.Should().Be(17);
+			autoResSlim1.Waiters.Should().Be(34);
+
+			autoResSlim1.TryAtomicXor(1, AutoResetSuperSlimLock.SignalledStatePos, AutoResetSuperSlimLock.SignalledStateMask);
+
+			autoResSlim1.IsSet.Should().BeTrue();
+			autoResSlim1.ToRelease.Should().Be(17);
+			autoResSlim1.Waiters.Should().Be(34);
+
+			autoResSlim1.TryAtomicXor(0, AutoResetSuperSlimLock.SignalledStatePos, AutoResetSuperSlimLock.SignalledStateMask);
+			autoResSlim1.IsSet.Should().BeFalse();
+
+			autoResSlim1.AtomicChange(1, AutoResetSuperSlimLock.SignalledStatePos, AutoResetSuperSlimLock.SignalledStateMask);
+			autoResSlim1.IsSet.Should().BeTrue();
+
+			autoResSlim1.AtomicChange(0, AutoResetSuperSlimLock.SignalledStatePos, AutoResetSuperSlimLock.SignalledStateMask);
+			autoResSlim1.IsSet.Should().BeFalse();
+			autoResSlim1.ToRelease.Should().Be(17);
+			autoResSlim1.Waiters.Should().Be(34);
+		}
+
+		[Test]
+		public void IsSet_ReflectsState()
+		{
+			var autoResSlim1 = new AutoResetSuperSlimLock(true);
+			var autoResSlim2 = new AutoResetSuperSlimLock(false);
+
+			autoResSlim1.IsSet.Should().BeTrue();
+			autoResSlim2.IsSet.Should().BeFalse();
+		}
+
+		[Test]
+		public void Wait_WhenAlreadySet_ReturnsAndResets()
+		{
+			var autoResSlim = new AutoResetSuperSlimLock(true);
+
+			autoResSlim.Wait().Should().BeTrue();
+			autoResSlim.Wait(0).Should().BeFalse();
+
+			autoResSlim.IsSet.Should().BeFalse();
+		}
+
+		[Test]
+		public void ConcurrentWaits_1()
+		{
+			var autoResSlim = new AutoResetSuperSlimLock(true);
+
+			var barrier = new ManualResetEvent(false);
+
+			int countOfObtained = 0;
+			var tasks = new List<Task>();
+
+			for (int i = 0; i < 4; i++)
+			{
+				var t = Task.Factory.StartNew(() =>
+				{
+					barrier.WaitOne();
+
+					if (autoResSlim.Wait(100)) Interlocked.Increment(ref countOfObtained);
+				});
+				tasks.Add(t);
+			}
+
+			barrier.Set();
+
+			Task.WaitAll(tasks.ToArray());
+
+			autoResSlim.IsSet.Should().BeFalse();
+
+			countOfObtained.Should().Be(1);
+		}
+
+		[Test]
+		public void ConcurrentWaits_2()
+		{
+			var autoResSlim = new AutoResetSuperSlimLock(false);
+
+			var barrier = new ManualResetEvent(false);
+
+			int countOfObtained = 0;
+			var tasks = new List<Task>();
+			var _run = true;
+
+			for (int i = 0; i < 4; i++)
+			{
+				var t = Task.Factory.StartNew(() =>
+				{
+					barrier.WaitOne();
+
+					while (_run)
+					{
+						if (autoResSlim.Wait(100))
+						{
+							Interlocked.Increment(ref countOfObtained);
+							break;
+						}
+					}
+				});
+				tasks.Add(t);
+			}
+			
+			barrier.Set();
+
+			Thread.Sleep(500);
+			
+			autoResSlim.Set();
+			
+			Thread.Sleep(1);
+			_run = false;
+
+			Task.WaitAll(tasks.ToArray());
+
+			countOfObtained.Should().Be(1);
+			autoResSlim.IsSet.Should().BeFalse();
+		}
+
+		[Test]
+		public void ConcurrentWaits__withReleaseAll_1()
+		{
+			var autoResSlim = new AutoResetSuperSlimLock(false, releaseAllOnSet: true);
+
+			var barrier = new ManualResetEvent(false);
+
+			int countOfObtained = 0;
+			var tasks = new List<Task>();
+
+			for (int i = 0; i < 12; i++)
+			{
+				var t = Task.Factory.StartNew(() =>
+				{
+					barrier.WaitOne();
+
+					if (autoResSlim.Wait(100000)) Interlocked.Increment(ref countOfObtained);
+				});
+				tasks.Add(t);
+			}
+
+			barrier.Set();
+
+			Thread.Sleep(2000);
+
+			autoResSlim.Set();
+
+			Task.WaitAll(tasks.ToArray());
+
+			countOfObtained.Should().Be(12);
+
+			autoResSlim.IsSet.Should().BeFalse();
+		}
+
+		[Test]
+		public async void ConcurrentSetsAndWaits__3()
+		{
+			var autoResSlim = new AutoResetSuperSlimLock(false);
+
+			var obtainedEv = new AutoResetEvent(true);
+			var barrier = new ManualResetEvent(false);
+			var semaphore = new SemaphoreSlim(0, 4);
+
+			int countOfObtained = 0;
+			int countOfMissed = 0;
+			var tasks = new List<Task>();
+
+			for (int i = 0; i < 4; i++)
+			{
+				var t = Task.Factory.StartNew(async (p) =>
+				{
+					barrier.WaitOne();
+
+					for (int j = 0; j < 1000; j++)
+					{
+						if (autoResSlim.Wait())
+						{
+							Interlocked.Increment(ref countOfObtained);
+							Console.WriteLine(countOfObtained + " _ " + p);
+							obtainedEv.Set();
+						}
+						else
+						{
+							Interlocked.Increment(ref countOfMissed);
+						}
+					}
+
+					semaphore.Release();
+				}, i);
+				tasks.Add(t);
+			}
+
+			var t1 = Task.Factory.StartNew(async () =>
+			{
+				barrier.Set();
+				for (int i = 0; i < 4000; i++)
+				{
+					obtainedEv.WaitOne();
+					autoResSlim.Set();
+					Console.WriteLine("*\t" + i);
+				}
+			});
+			tasks.Add(t1);
+
+			Task.WaitAll(tasks.ToArray());
+
+			for (int i = 0; i < 4; i++)
+				semaphore.Wait();
+
+			countOfObtained.Should().Be(4000);
+
+			autoResSlim.IsSet.Should().BeFalse();
+		}
+	}
+}
---
+++ src/RabbitMqNext/Internals/RingBuffer/Locks/AutoResetSuperSlimLock.cs
@@ -0,0 +1,279 @@
+namespace RabbitMqNext.Internals.RingBuffer.Locks
+{
+	using System;
+	using System.Collections.Concurrent;
+	using System.Collections.Generic;
+	using System.Runtime.CompilerServices;
+	using System.Threading;
+	using System.Threading.Tasks;
+
+
+	/// <summary>
+	/// heavily "inspired" by the counterpart ManualResetEventSlim
+	/// with the difference that waiters try to atomically "unset" the set bit/flag
+	/// </summary>
+	internal class AutoResetSuperSlimLock
+	{
+		internal const int SignalledStateMask = 0x8000;      // 1000 0000 0000 0000
+		internal const int SignalledStatePos = 15;
+		internal const int NumWaitersStateMask = (0xFF);     // 0000 0000 1111 1111
+		internal const int NumWaitersStatePos = 0;
+		internal const int RelWaitersStateMask = (0x7F00);   // 0111 1111 0000 0000
+		internal const int RelWaitersStatePos = 8;
+
+		private readonly ConcurrentQueue<TaskCompletionSource<bool>> _waiters = new ConcurrentQueue<TaskCompletionSource<bool>>();
+		private volatile int _state;
+		private readonly object _lock = new object();
+		
+		private static readonly int ProcCounter = Environment.ProcessorCount;
+
+		private const int HowManySpinBeforeYield = 10;
+		private const int HowManyYieldEverySleep0 = 5;
+		private const int HowManyYieldEverySleep1 = 20;
+
+		public AutoResetSuperSlimLock(bool initialState = false, bool releaseAllOnSet = false)
+		{
+			if (initialState) _state = SignalledStateMask;
+		}
+
+		public Task WaitAsync()
+		{
+			return WaitAsync(Timeout.Infinite);
+		}
+
+		public Task WaitAsync(int millisecondsTimeout) //, CancellationToken cancellationToken)
+		{
+//			cancellationToken.Register(OnCancelled)
+
+			if (!CheckForIsSetAndResetIfTrue())
+			{
+				if (SpinAndTryToObtainLock()) 
+					return Task.CompletedTask;
+
+				var tcs = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+				_waiters.Enqueue(tcs);
+			}
+
+			return Task.CompletedTask;
+		}
+
+		public bool Wait()
+		{
+			return Wait(Timeout.Infinite);
+		}
+
+		public bool Wait(int millisecondsTimeout)
+		{
+			if (!CheckForIsSetAndResetIfTrue())
+			{
+				if (SpinAndTryToObtainLock()) 
+					return true;
+
+				lock (_lock)
+				{
+					// if (!CheckForIsSetAndResetIfTrue())
+					{
+						Waiters++;
+
+						if (CheckForIsSetAndResetIfTrue())
+						{
+							Waiters--;
+							return true;
+						}
+
+						try
+						{
+							while (true)
+							{
+								if (!Monitor.Wait(_lock, millisecondsTimeout))
+								{
+									return false; // timeout expired
+								}
+								else
+								{
+									if (CheckForIsSetAndResetIfTrue())
+										break;
+								}
+							}
+						}
+						finally
+						{
+							Waiters--;
+						}
+					}
+				}
+			}
+
+			return true;
+		}
+
+		public void Set()
+		{
+			AtomicChange(1, SignalledStatePos, SignalledStateMask);
+
+			var tcss = new List<TaskCompletionSource<bool>>();
+
+			lock (_lock)
+			{
+				if (Waiters > 0)
+				{
+					Monitor.Pulse(_lock);
+				}
+				else
+				{
+					do
+					{
+						TaskCompletionSource<bool> tcs;
+						if (_waiters.TryDequeue(out tcs))
+						{
+							AtomicChange(0, SignalledStatePos, SignalledStateMask);
+							tcss.Add(tcs);
+						}
+						else break;
+
+					} while (!_waiters.IsEmpty);
+				}
+			}
+			// schedules the continuation to run
+			// since it was created with TaskCreationOptions.RunContinuationsAsynchronously
+			// it's guaranteed to run outside this _lock, but just in case...
+			foreach (var tcs in tcss)
+			{
+				tcs.SetResult(true);
+			}
+		}
+
+		public bool IsSet
+		{
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			get { return (_state & SignalledStateMask) != 0; }
+		}
+
+		public void Dispose()
+		{
+			TaskCompletionSource<bool> tcs;
+			if (_waiters.TryDequeue(out tcs))
+				tcs.SetCanceled();
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private bool CheckForIsSetAndResetIfTrue()
+		{
+			return TryAtomicXor(0, SignalledStatePos, SignalledStateMask);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		internal void AtomicChange(int val, int shifts, int mask)
+		{
+			var spinWait = new SpinWait();
+			while (true)
+			{
+				var curState = _state;
+
+				// (1) zero the updateBits.  eg oldState = [11111111]    flag=00111000  newState= [11000111]
+				// (2) map in the newBits.              eg [11000111] newBits=00101000, newState= [11101111]
+				int newState = (curState & ~mask) | (val << shifts);
+
+#pragma warning disable 420
+				if (Interlocked.CompareExchange(ref _state, newState, curState) == curState)
+#pragma warning restore 420
+				{
+					break;
+				}
+
+				spinWait.SpinOnce();
+			}
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		internal bool TryAtomicXor(int val, int shifts, int mask)
+		{
+			var curState = _state;
+
+			// (1) zero the updateBits.  eg oldState = [11111111]    flag=00111000  newState= [11000111]
+			// (2) map in the newBits.              eg [11000111] newBits=00101000, newState= [11101111]
+			int newState = (curState & ~mask) |  (val << shifts);
+
+			// (1) zero the updateBits.  eg oldState = [11111111]    flag=00111000  newState= [11000111]
+			// (2) map in the newBits.              eg [11000111] newBits=00101000, newState= [11101111]
+			int expected = (newState ^ mask) | curState;
+
+			// newState [100001]
+			// expected [000001]
+
+#pragma warning disable 420
+			return (Interlocked.CompareExchange(ref _state, newState, expected) == expected);
+#pragma warning restore 420
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private bool SpinAndTryToObtainLock()
+		{
+			const int spinCount = 10;
+
+			for (int i = 0; i < spinCount; i++)
+			{
+				if (CheckForIsSetAndResetIfTrue())
+				{
+					return true;
+				}
+				if (i < HowManySpinBeforeYield)
+				{
+					if (i == HowManySpinBeforeYield / 2)
+					{
+						Thread.Yield();
+					}
+					else
+					{
+						Thread.SpinWait(ProcCounter * (4 << i));
+					}
+				}
+				else if (i % HowManyYieldEverySleep1 == 0)
+				{
+					Thread.Sleep(1);
+				}
+				else if (i % HowManyYieldEverySleep0 == 0)
+				{
+					Thread.Sleep(0);
+				}
+				else
+				{
+					Thread.Yield();
+				}
+			}
+			return CheckForIsSetAndResetIfTrue();
+		}
+
+		internal int Waiters
+		{
+			get
+			{
+				return ExtractStatePortionAndShiftRight(_state, NumWaitersStateMask, NumWaitersStatePos);
+			}
+			set
+			{
+				AtomicChange(value, NumWaitersStatePos, NumWaitersStateMask);
+			}
+		}
+
+		internal int ToRelease
+		{
+			get
+			{
+				var val = ExtractStatePortionAndShiftRight(_state, RelWaitersStateMask, RelWaitersStatePos);
+				return val;
+			}
+			set
+			{
+				AtomicChange(value, RelWaitersStatePos, RelWaitersStateMask);
+			}
+		}
+
+		private static int ExtractStatePortionAndShiftRight(int state, int mask, int rightBitShiftCount)
+		{
+			//convert to uint before shifting so that right-shift does not replicate the sign-bit,
+			//then convert back to int.
+			return unchecked((int)(((uint)(state & mask)) >> rightBitShiftCount));
+		}
+	}
+}
