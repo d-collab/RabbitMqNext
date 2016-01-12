2903d414e6c1f1ca79795cfc3e835752e053256f
hammett <hammett@gmail.com>
2016-01-11 22:32:01 -0800

    split file

---
+++ src/RabbitMqNext/Internals/AmqpClassMethodChannelLevelConstants.cs
@@ -0,0 +1,46 @@
+namespace RabbitMqNext.Internals
+{
+	public static class AmqpClassMethodChannelLevelConstants
+	{
+		public const int ChannelOpenOk = (20 << 16) | 11;
+
+		public const int ChannelFlow = (20 << 16) | 20;
+
+		public const int ChannelFlowOk = (20 << 16) | 21;
+
+		public const int ChannelClose = (20 << 16) | 40;
+
+		public const int ChannelCloseOk = (20 << 16) | 41;
+
+		public const int ExchangeDeclareOk = (40 << 16) | 11;
+
+		public const int ExchangeBindOk = (40 << 16) | 31;
+
+		public const int ExchangeUnbindOk = (40 << 16) | 51;
+
+		public const int QueueDeclareOk = (50 << 16) | 11;
+
+		public const int QueueBindOk = (50 << 16) | 21;
+
+		public const int QueueUnbindOk = (50 << 16) | 51;
+
+		public const int BasicQosOk = (60 << 16) | 11;
+
+		public const int BasicConsumeOk = (60 << 16) | 21;
+
+		public const int BasicReturn = (60 << 16) | 50;
+
+		public const int BasicDeliver = (60 << 16) | 60;
+
+		public const int ConfirmSelectOk = (85 << 16) | 11;
+
+		public const int BasicAck = (60 << 16) | 80;
+
+		public const int BasicNAck = (60 << 16) | 120;
+
+		public const int BasicReject = (60 << 16) | 90;
+
+		public const int RecoverOk = (60 << 16) | 111;
+
+	}
+}
--- src/RabbitMqNext/Internals/AmqpClassMethodConnectionLevelConstants.cs
+++ src/RabbitMqNext/Internals/AmqpClassMethodConnectionLevelConstants.cs
@@ -2,51 +2,20 @@ namespace RabbitMqNext.Internals
 {
 	public static class AmqpClassMethodConnectionLevelConstants
 	{
-		public const int ConnectionStart = 655370;
+		public const int ConnectionStart = (10 << 16) | 10;
 
-		public const int ConnectionStartOk = 655371;
+		public const int ConnectionStartOk = (10 << 16) | 11;
 
-		public const int ConnectionTune = 655390;
+		public const int ConnectionTune = (10 << 16) | 30;
 
-		public const int ConnectionClose = 655410;
+		public const int ConnectionOpenOk = (10 << 16) | 41;
 
-		public const int ConnectionOpenOk = 655401;
+		public const int ConnectionClose = (10 << 16) | 50;
 
 		public const int ConnectionCloseOk = (10 << 16) | 51;
-	}
 
-	public static class AmqpClassMethodChannelLevelConstants
-	{
-		public const int ChannelOpenOk = (20 << 16) | 11;
+		public const int ConnectionBlocked = (10 << 16) | 60;
 
-		public const int ChannelFlowOk = (20 << 16) | 21;
-
-		public const int ChannelClose = (20 << 16) | 40;
-
-		public const int ChannelCloseOk = (20 << 16) | 41;
-
-		public const int ExchangeDeclareOk = (40 << 16) | 11;
-
-		public const int ExchangeBindOk = (40 << 16) | 31;
-
-		public const int ExchangeUnbindOk = (40 << 16) | 51;
-
-		public const int QueueDeclareOk = (50 << 16) | 11;
-
-		public const int QueueBindOk = (50 << 16) | 21;
-
-		public const int QueueUnbindOk = (50 << 16) | 51;
-
-		public const int BasicQosOk = (60 << 16) | 11;
-
-		public const int BasicConsumeOk = (60 << 16) | 21;
-
-//		public const int BasicPublish = (60 << 16) | 40;
-
-		public const int BasicReturn = (60 << 16) | 50;
-
-		public const int BasicDeliver = (60 << 16) | 60;
-
-		// public const int BasicAck = (60 << 16) | 80;
+		public const int ConnectionUnblocked = (10 << 16) | 61;
 	}
 }
