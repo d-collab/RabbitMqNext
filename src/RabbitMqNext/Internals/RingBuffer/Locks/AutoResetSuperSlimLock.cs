fc0fa81a4ceba240ae679cd535424caf4549cf15
hammett <hammett@gmail.com>
2016-01-15 18:41:05 -0800

    re-enable spining

--- src/RabbitMqNext/Internals/RingBuffer/Locks/AutoResetSuperSlimLock.cs
+++ src/RabbitMqNext/Internals/RingBuffer/Locks/AutoResetSuperSlimLock.cs
@@ -210,30 +210,30 @@ namespace RabbitMqNext.Internals.RingBuffer.Locks
 				{
 					return true;
 				}
-				Thread.SpinWait(1 << i);
-//				if (i < HowManySpinBeforeYield)
-//				{
-//					if (i == HowManySpinBeforeYield / 2)
-//					{
-//						Thread.Yield();
-//					}
-//					else
-//					{
-//						Thread.SpinWait(ProcCounter * (4 << i));
-//					}
-//				}
-//				else if (i % HowManyYieldEverySleep1 == 0)
-//				{
-//					Thread.Sleep(1);
-//				}
-//				else if (i % HowManyYieldEverySleep0 == 0)
-//				{
-//					Thread.Sleep(0);
-//				}
-//				else
-//				{
-//					Thread.Yield();
-//				}
+//				Thread.SpinWait(1 << i);
+				if (i < HowManySpinBeforeYield)
+				{
+					if (i == HowManySpinBeforeYield / 2)
+					{
+						Thread.Yield();
+					}
+					else
+					{
+						Thread.SpinWait(ProcCounter * (4 << i));
+					}
+				}
+				else if (i % HowManyYieldEverySleep1 == 0)
+				{
+					Thread.Sleep(1);
+				}
+				else if (i % HowManyYieldEverySleep0 == 0)
+				{
+					Thread.Sleep(0);
+				}
+				else
+				{
+					Thread.Yield();
+				}
 			}
 			return CheckForIsSetAndResetIfTrue();
 		}
