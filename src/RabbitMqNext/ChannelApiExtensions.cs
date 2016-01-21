123b9bbe14011255ac152e6e9d92d13a7fd75be1
hammett <hammett@gmail.com>
2016-01-21 00:10:22 -0800

    api extensions

--- src/RabbitMqNext/ChannelApiExtensions.cs
+++ src/RabbitMqNext/ChannelApiExtensions.cs
@@ -1,8 +1,35 @@
 namespace RabbitMqNext
 {
+	using System;
+
 	public static class ChannelApiExtensions
 	{
-		
+		public static TaskSlim BasicPublish(this Channel source, 
+			string exchange, string routingKey, bool mandatory, bool immediate,
+			BasicProperties properties, byte[] buffer)
+		{
+			return source.BasicPublish(exchange, routingKey, mandatory, immediate, properties, new ArraySegment<byte>(buffer));
+		}
 
+		public static TaskSlim BasicPublish(this Channel source,
+			string exchange, string routingKey, bool mandatory,
+			BasicProperties properties, byte[] buffer)
+		{
+			return source.BasicPublish(exchange, routingKey, mandatory, false, properties, new ArraySegment<byte>(buffer));
+		}
+
+		public static void BasicPublishFast(this Channel source,
+			string exchange, string routingKey, bool mandatory, bool immediate,
+			BasicProperties properties, byte[] buffer)
+		{
+			source.BasicPublishFast(exchange, routingKey, mandatory, immediate, properties, new ArraySegment<byte>(buffer));
+		}
+
+		public static void BasicPublishFast(this Channel source,
+			string exchange, string routingKey, bool mandatory,
+			BasicProperties properties, byte[] buffer)
+		{
+			source.BasicPublishFast(exchange, routingKey, mandatory, false, properties, new ArraySegment<byte>(buffer));
+		}
 	}
 }
