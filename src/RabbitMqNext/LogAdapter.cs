dba149b18cff1af709123caac9d00b4beba2e641
hammett <hammett@gmail.com>
2016-02-20 03:17:07 -0800

    Adding LogAdapter to bridge logging libraries

---
+++ src/RabbitMqNext/LogAdapter.cs
@@ -0,0 +1,74 @@
+﻿namespace RabbitMqNext
+{
+	using System;
+	using System.Diagnostics;
+
+	/// <summary>
+	/// This is a hack to not carry a dependency on a particular logging library.
+	/// Set the delegates to bridge to your favorite 
+	/// logging library and set ExtendedLogEnabled to true. 
+	/// </summary>
+	public static class LogAdapter
+	{
+		public static bool ExtendedLogEnabled;
+
+		// signature: class, message, [exception]
+		public static Action<string, string, Exception> LogDebugFn { get; set; }
+		public static Action<string, string, Exception> LogErrorFn { get; set; }
+		public static Action<string, string, Exception> LogWarnFn { get; set; }
+
+		static LogAdapter()
+		{
+			LogDebugFn = (c, m, ex) =>
+			{
+				var msg = string.Format("Debug {0}: {1}", c, m);
+				Console.Out.WriteLine(msg);
+				if (Debugger.IsLogging())
+				{
+					Debugger.Log(5, "DEBUG", msg);
+				}
+			};
+
+			LogErrorFn = (c, m, ex) =>
+			{
+				var msg = string.Format("Error {0}: {1}", c, m);
+				Console.Error.WriteLine(msg);
+				if (Debugger.IsLogging())
+				{
+					Debugger.Log(1, "ERROR", msg);
+				}
+			};
+
+			LogWarnFn = (c, m, ex) =>
+			{
+				var msg = string.Format("Warn {0}: {1}", c, m);
+				Console.Out.WriteLine(msg);
+				if (Debugger.IsLogging())
+				{
+					Debugger.Log(3, "WARN", msg);
+				}
+			};
+		}
+
+
+		public static void LogDebug(string context, string message)
+		{
+			LogDebugFn(context, message, null);
+		}
+
+		public static void LogDebug(string context, string message, Exception ex)
+		{
+			LogDebugFn(context, message, ex);
+		}
+
+		public static void LogError(string context, string message, Exception ex)
+		{
+			LogErrorFn(context, message, ex);
+		}
+
+		public static void LogWarn(string context, string message, Exception ex = null)
+		{
+			LogWarnFn(context, message, ex);
+		}
+	}
+}
