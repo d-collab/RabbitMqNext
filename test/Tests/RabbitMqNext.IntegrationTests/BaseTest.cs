a460e81cbc09b091228c8d79eccf9f255a8ce5a8
hammett <hammett@gmail.com>
2016-02-23 00:26:13 -0800

    making sure vhost exists

--- test/Tests/RabbitMqNext.IntegrationTests/BaseTest.cs
+++ test/Tests/RabbitMqNext.IntegrationTests/BaseTest.cs
@@ -1,7 +1,9 @@
 namespace RabbitMqNext.IntegrationTests
 {
 	using System.Configuration;
+	using System.Linq;
 	using System.Threading.Tasks;
+	using MConsole;
 	using NUnit.Framework;
 
 	public class BaseTest
@@ -14,6 +16,13 @@ namespace RabbitMqNext.IntegrationTests
 			var vhost = ConfigurationManager.AppSettings["vhost"];
 			var username = ConfigurationManager.AppSettings["username"];
 			var password = ConfigurationManager.AppSettings["password"];
+
+			var console = new RestConsole(host, username, password);
+			var vhosts = await console.GetVHosts();
+			if (vhosts.Any(v => v.Name == vhost))
+			{
+				await console.CreateVHost(vhost);
+			}
 
 			var conn = await new ConnectionFactory().Connect(host, vhost, username, password);
 			_conn = conn;
