327e6eee60fdb6b5756c20cd9edea5d189afc8ad
hammett <hammett@gmail.com>
2016-02-25 19:00:58 -0800

    fixed extensions methods

--- src/RabbitMqNext/ChannelApiExtensions.cs
+++ src/RabbitMqNext/ChannelApiExtensions.cs
@@ -4,14 +4,14 @@ namespace RabbitMqNext
 
 	public static class ChannelApiExtensions
 	{
-		public static TaskSlim BasicPublish(this Channel source, 
+		public static TaskSlim BasicPublish(this IChannel source, 
 			string exchange, string routingKey, bool mandatory, 
 			BasicProperties properties, byte[] buffer)
 		{
 			return source.BasicPublish(exchange, routingKey, mandatory, properties, new ArraySegment<byte>(buffer));
 		}
 
-		public static TaskSlim BasicPublish(this Channel source,
+		public static TaskSlim BasicPublish(this IChannel source,
 			string exchange, string routingKey, 
 			BasicProperties properties, byte[] buffer)
 		{
@@ -19,14 +19,14 @@ namespace RabbitMqNext
 				properties: properties, buffer: new ArraySegment<byte>(buffer));
 		}
 
-		public static void BasicPublishFast(this Channel source,
+		public static void BasicPublishFast(this IChannel source,
 			string exchange, string routingKey, bool mandatory, 
 			BasicProperties properties, byte[] buffer)
 		{
 			source.BasicPublishFast(exchange, routingKey, mandatory, properties, new ArraySegment<byte>(buffer));
 		}
 
-		public static void BasicPublishFast(this Channel source,
+		public static void BasicPublishFast(this IChannel source,
 			string exchange, string routingKey, 
 			BasicProperties properties, byte[] buffer)
 		{
