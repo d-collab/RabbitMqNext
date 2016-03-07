c23e407eb7113613263552427c06ae8e24d0911f
hammett <hammett@gmail.com>
2016-03-06 22:43:00 -0800

    recovery support WIP

---
+++ src/RabbitMqNext/Recovery/AutoRecoveryEnabledChannel.cs
@@ -0,0 +1,177 @@
+﻿namespace RabbitMqNext
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Threading.Tasks;
+	using Internals;
+
+	public class AutoRecoveryEnabledChannel : IChannel
+	{
+		const string LogSource = "ChannelRecovery";
+		
+		private readonly Channel _channel;
+
+		public AutoRecoveryEnabledChannel(Channel channel)
+		{
+			_channel = channel;
+		}
+
+		#region Implementation of IChannel
+
+		public event Action<AmqpError> OnError
+		{
+			add { _channel.OnError += value; }
+			remove { _channel.OnError -= value; }
+		}
+		
+		public Func<UndeliveredMessage, Task> MessageUndeliveredHandler 
+		{ 
+			get { return _channel.MessageUndeliveredHandler; } 
+			set { _channel.MessageUndeliveredHandler = value; } 
+		}
+		
+		public bool IsConfirmationEnabled
+		{
+			get { return _channel.IsConfirmationEnabled; }
+		}
+		
+		public ushort ChannelNumber
+		{
+			get { return _channel.ChannelNumber; }
+		}
+
+		public bool IsClosed
+		{
+			get { return _channel.IsClosed; }
+		}
+
+		public BasicProperties RentBasicProperties()
+		{
+			return _channel.RentBasicProperties();
+		}
+
+		public void Return(BasicProperties properties)
+		{
+			_channel.Return(properties);
+		}
+
+		public Task BasicQos(uint prefetchSize, ushort prefetchCount, bool global)
+		{
+			return null;
+		}
+
+		public void BasicAck(ulong deliveryTag, bool multiple)
+		{
+		}
+
+		public void BasicNAck(ulong deliveryTag, bool multiple, bool requeue)
+		{
+		}
+
+		public Task ExchangeDeclare(string exchange, string type, bool durable, bool autoDelete, IDictionary<string, object> arguments,
+			bool waitConfirmation)
+		{
+			return null;
+		}
+
+		public Task ExchangeBind(string source, string destination, string routingKey, IDictionary<string, object> arguments, bool waitConfirmation)
+		{
+			return null;
+		}
+
+		public Task ExchangeUnbind(string source, string destination, string routingKey, IDictionary<string, object> arguments, bool waitConfirmation)
+		{
+			return null;
+		}
+
+		public Task ExchangeDelete(string exchange, IDictionary<string, object> arguments, bool waitConfirmation)
+		{
+			return null;
+		}
+
+		public Task<AmqpQueueInfo> QueueDeclare(string queue, bool passive, bool durable, bool exclusive, bool autoDelete, IDictionary<string, object> arguments,
+			bool waitConfirmation)
+		{
+			return null;
+		}
+
+		public Task QueueBind(string queue, string exchange, string routingKey, IDictionary<string, object> arguments, bool waitConfirmation)
+		{
+			return null;
+		}
+
+		public Task QueueUnbind(string queue, string exchange, string routingKey, IDictionary<string, object> arguments)
+		{
+			return null;
+		}
+
+		public Task QueueDelete(string queue, bool waitConfirmation)
+		{
+			return null;
+		}
+
+		public Task QueuePurge(string queue, bool waitConfirmation)
+		{
+			return null;
+		}
+
+		public TaskSlim BasicPublishWithConfirmation(string exchange, string routingKey, bool mandatory, BasicProperties properties,
+			ArraySegment<byte> buffer)
+		{
+			return null;
+		}
+
+		public TaskSlim BasicPublish(string exchange, string routingKey, bool mandatory, BasicProperties properties,
+			ArraySegment<byte> buffer)
+		{
+			return null;
+		}
+
+		public void BasicPublishFast(string exchange, string routingKey, bool mandatory, BasicProperties properties,
+			ArraySegment<byte> buffer)
+		{
+		}
+
+		public Task<string> BasicConsume(ConsumeMode mode, QueueConsumer consumer, string queue, string consumerTag, bool withoutAcks,
+			bool exclusive, IDictionary<string, object> arguments, bool waitConfirmation)
+		{
+			return null;
+		}
+
+		public Task<string> BasicConsume(ConsumeMode mode, Func<MessageDelivery, Task> consumer, string queue, string consumerTag, bool withoutAcks, bool exclusive,
+			IDictionary<string, object> arguments, bool waitConfirmation)
+		{
+			return null;
+		}
+
+		public Task BasicCancel(string consumerTag, bool waitConfirmation)
+		{
+			return null;
+		}
+
+		public Task BasicRecover(bool requeue)
+		{
+			return null;
+		}
+
+		public Task<RpcHelper> CreateRpcHelper(ConsumeMode mode, int? timeoutInMs, int maxConcurrentCalls = 500)
+		{
+			return null;
+		}
+
+		public Task Close()
+		{
+			return null;
+		}
+
+		#endregion
+
+		#region Implementation of IDisposable
+
+		public void Dispose()
+		{
+		}
+
+		#endregion
+	}
+}
---
+++ src/RabbitMqNext/Recovery/AutoRecoveryEnabledConnection.cs
@@ -0,0 +1,196 @@
+﻿namespace RabbitMqNext
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Threading;
+	using System.Threading.Tasks;
+	using Internals;
+	using Internals.RingBuffer;
+
+	internal enum ConnectionRecoveryAction
+	{
+		NoAction,
+		WillReconnect
+	}
+
+	public class AutoRecoveryEnabledConnection : IConnection
+	{
+		const string LogSource = "ConnectionRecovery";
+
+		private readonly IEnumerable<string> _hostnames;
+		private readonly Connection _connection;
+		private readonly List<AutoRecoveryEnabledChannel> _channelRecoveries;
+		private volatile bool _disableRecovery;
+		private string _selectedHostname;
+
+		public AutoRecoveryEnabledConnection(string hostname, Connection connection) 
+			: this(new [] { hostname }, connection)
+		{
+		}
+
+		public AutoRecoveryEnabledConnection(IEnumerable<string> hostnames, Connection connection)
+		{
+			_hostnames = hostnames;
+			_connection = connection;
+			_connection.Recovery = this;
+			_channelRecoveries = new List<AutoRecoveryEnabledChannel>();
+		}
+
+		internal AutoRecoveryEnabledChannel CreateChannelRecovery(Channel channel)
+		{
+			var channelRecovery = new AutoRecoveryEnabledChannel(channel);
+			lock (_channelRecoveries)
+				_channelRecoveries.Add(channelRecovery);
+			return channelRecovery;
+		}
+
+		public void NotifyConnected(string hostname)
+		{
+			_selectedHostname = hostname;
+
+			LogAdapter.LogDebug(LogSource, "Connected to " + hostname);
+		}
+
+		private int _inRecovery;
+
+		internal ConnectionRecoveryAction NotifyAbruptClose(Exception reason)
+		{
+			if (_disableRecovery)
+			{
+				LogAdapter.LogDebug(LogSource, "NotifyAbrupt skipping action since disconect was initiated by the user", reason);
+				return ConnectionRecoveryAction.NoAction;
+			}
+
+			LogAdapter.LogDebug(LogSource, "NotifyAbrupt", reason);
+
+			TryInitiateRecovery();
+
+			return ConnectionRecoveryAction.WillReconnect;
+		}
+
+		public void NotifyCloseByUser()
+		{
+			_disableRecovery = true;
+
+			LogAdapter.LogDebug(LogSource, "NotifyCloseByUser ");
+		}
+
+		public void NotifyCloseByServer()
+		{
+			// should we reconnect after connection error?
+
+			LogAdapter.LogDebug(LogSource, "NotifyCloseByServer ");
+		}
+
+		#region Implementation of IConnection
+
+		public event Action<AmqpError> OnError
+		{
+			add { _connection.OnError += value; }
+			remove { _connection.OnError -= value; }
+		}
+
+		public bool IsClosed
+		{
+			get { return _connection.IsClosed; }
+		}
+		
+		public async Task<IChannel> CreateChannel()
+		{
+			return CreateChannelRecovery(await _connection.CreateChannel() as Channel);
+		}
+
+		public async Task<IChannel> CreateChannelWithPublishConfirmation(int maxunconfirmedMessages = 100)
+		{
+			return CreateChannelRecovery(await _connection.CreateChannelWithPublishConfirmation(maxunconfirmedMessages) as Channel);
+		}
+
+		public void Dispose()
+		{
+			if (LogAdapter.ExtendedLogEnabled)
+				LogAdapter.LogDebug(LogSource, "Dispose called");
+
+			_disableRecovery = true;
+			
+			_connection.Dispose();
+		}
+
+		#endregion
+
+		private void TryInitiateRecovery()
+		{
+			if (Interlocked.CompareExchange(ref _inRecovery, 1, 0) == 0)
+			{
+				LogAdapter.LogDebug(LogSource, "TryInitiateRecovery starting recovery process");
+
+				ThreadFactory.BackgroundThread<AutoRecoveryEnabledConnection>(async (pthis) =>
+				{
+					try
+					{
+						await pthis.CycleReconnect();
+						await pthis.Recover();
+					}
+					catch (Exception ex)
+					{
+						LogAdapter.LogError(LogSource, "TryInitiateRecovery error", ex);
+						pthis.HandleRecoveryFatalError();
+					}
+					finally
+					{
+						Interlocked.Exchange(ref pthis._inRecovery, 0);
+					}
+
+				}, "RecoveryProc", this);
+			}
+			else
+			{
+				LogAdapter.LogDebug(LogSource, "TryInitiateRecovery: recovery in progress. skipping");
+			}
+		}
+
+		// Runs from a background thread
+		private async Task<bool> CycleReconnect()
+		{
+			var hosts = _hostnames.ToArray();
+			var index = 0;
+			var firstRun = true;
+
+			// TODO: maxattempts or api hook to allow continuing/stopping
+			while (true)
+			{
+				var hostToTry = hosts[index++ % hosts.Length];
+
+				if (hostToTry == _selectedHostname && firstRun) // skip the same hostname, but only once
+				{
+					firstRun = false;
+					continue;
+				}
+
+				var succeeded = await _connection.InternalConnect(hostToTry);
+				if (succeeded)
+					return true;
+
+				// TODO: parameterized wait time
+				Thread.Sleep(1000);
+			}
+		}
+
+		private Task Recover()
+		{
+			// Recover channels
+			// Recover declares
+			// Recover bindings
+			// Recover subscriptions
+			// Recover rpchelpers
+
+			return Task.CompletedTask;
+		}
+
+		// When the recovery process completely failed, close everything
+		private void HandleRecoveryFatalError()
+		{
+			// TODO: implement this
+		}
+	}
+}
