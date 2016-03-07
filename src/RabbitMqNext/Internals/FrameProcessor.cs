c6e1a2a0face2178cac7b25419606a34a4e85803
hammett <hammett@gmail.com>
2016-03-06 22:42:13 -0800

    cleaned up

--- src/RabbitMqNext/Internals/FrameProcessor.cs
+++ src/RabbitMqNext/Internals/FrameProcessor.cs
@@ -5,11 +5,6 @@ namespace RabbitMqNext.Internals
 
 	public interface IFrameProcessor
 	{
-		// internal abstract Task DispatchMethod(ushort channel, int classMethodId);
 		Task DispatchMethod(ushort channel, int classMethodId);
-
-//		public abstract Task DispatchCloseMethod(ushort channel, ushort replyCode, string replyText, ushort classId, ushort methodId);
-//
-//		public abstract Task DispatchChannelCloseMethod(ushort channel, ushort replyCode, string replyText, ushort classId, ushort methodId);
 	}
 }
