09cc58ea3bf6ef819b3f13e3aee3b0ca291db29e
hammett <hammett@gmail.com>
2016-03-06 22:41:50 -0800

    initial autorecovery integration tests

---
+++ test/Tests/RabbitMqNext.IntegrationTests/ConnectionRecoveryTestCase.cs
@@ -0,0 +1,26 @@
+namespace RabbitMqNext.IntegrationTests
+{
+	using System;
+	using System.Threading.Tasks;
+	using FluentAssertions;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class ConnectionRecoveryTestCase : BaseTest
+	{
+		[Test]
+		public async Task ServerErrorShouldTriggerReconnect()
+		{
+			Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+			var conn = await base.StartConnection(autoRecovery: true);
+
+			conn._io._socketHolder.Writer.Write(123456);
+			conn._io._socketHolder.Writer.Write(123456);
+//			conn._io._socketHolder.Writer.Write(123456);
+//			conn._io._socketHolder.Writer.Write(123456);
+
+			await Task.Delay(1000);
+		}
+	}
+}
