9e72f501ed5100840707322ce1fc56b0a9230c36
hammett <hammett@gmail.com>
2016-04-08 14:49:20 -0700

    making consumer api know about recovery state changes

--- src/RabbitMqNext/QueueConsumer.cs
+++ src/RabbitMqNext/QueueConsumer.cs
@@ -7,5 +7,26 @@ namespace RabbitMqNext
 		public abstract Task Consume(MessageDelivery delivery);
 
 		// Add Connection lost, Cancelled, etc..
+
+		/// <summary>
+		/// When auto-recovery is enabled, indicates that the connection is down but recovery will start
+		/// </summary>
+		public virtual void Broken()
+		{
+		}
+
+		/// <summary>
+		/// When auto-recovery is enabled, indicates that recovery has succeeded
+		/// </summary>
+		public virtual void Recovered()
+		{
+		}
+
+		/// <summary>
+		/// indicates that the server cancelled this consumer
+		/// </summary>
+		public virtual void Cancelled()
+		{
+		}
 	}
 }
