def1cb93164fe2fc4ffd4fc52f54ab25f9243ebc
hammett <hammett@gmail.com>
2016-06-23 13:55:19 -0300

    assert utility

---
+++ src/RabbitMqNext/Utils/Asserts.cs
@@ -0,0 +1,23 @@
+﻿namespace RabbitMqNext.Utils
+{
+	using System;
+	using System.Diagnostics;
+	using System.Runtime.CompilerServices;
+	using Io;
+
+	internal static class Asserts
+	{
+		[DebuggerStepThrough]
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void AssertNotReadFrameThread()
+		{
+#if ASSERT
+			var name = System.Threading.Thread.CurrentThread.Name;
+			if (name != null && name.StartsWith(ConnectionIO.ReadFrameThreadNamePrefix, StringComparison.Ordinal))
+			{
+				LogAdapter.LogError("Asserts", "Assert failed: AssertNotReadFrameThread at " + new StackTrace());
+			}
+#endif
+		}
+	}
+}
