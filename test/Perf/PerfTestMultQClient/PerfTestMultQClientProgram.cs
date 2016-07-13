065673f2d36dc1f27a6c34bd5fdb41ead2b77dec
hammett <hammett@gmail.com>
2016-07-13 11:48:54 -0700

    perf test projs to compare multi queues rpc

---
+++ test/Perf/PerfTestMultQClient/PerfTestMultQClientProgram.cs
@@ -0,0 +1,134 @@
+﻿namespace PerfTestMultQClient
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Configuration;
+	using System.Diagnostics;
+	using System.Text;
+	using System.Threading;
+	using System.Threading.Tasks;
+	using RabbitMqNext;
+	using RabbitMQ.Client;
+
+	class PerfTestMultQClientProgram
+	{
+		private ManualResetEventSlim _startSync;
+
+		static void Main(string[] args)
+		{
+			new PerfTestMultQClientProgram().Run(args).Wait();
+		}
+
+		private async Task Run(string[] args)
+		{
+			// 
+			// client side 
+			// sends rpc requests, measures the roundtrip
+			// 
+
+
+			var host = ConfigurationManager.AppSettings["rabbit.host"];
+			var user = ConfigurationManager.AppSettings["rabbit.admuser"];
+			var pwd = ConfigurationManager.AppSettings["rabbit.admpwd"];
+			var vhost = ConfigurationManager.AppSettings["rabbit.vhost"];
+
+
+			int howManyQueues = 2;
+			bool exclusiveConnections = ConfigurationManager.AppSettings["exclusiveConnections"] == "true";
+			bool useOfficialClient = ConfigurationManager.AppSettings["useOfficialClient"] == "true";
+
+			var howManyCalls = 100;
+			var tasks = new List<Task>();
+			_startSync = new ManualResetEventSlim(false);
+
+			if (useOfficialClient)
+			{
+				RabbitMQ.Client.IConnection conn = null;
+				RabbitMQ.Client.IModel channel = null;
+
+				for (int i = 0; i < howManyQueues; i++)
+				{
+					var connFac = new RabbitMQ.Client.ConnectionFactory() { HostName = host, UserName = user, Password = pwd, VirtualHost = vhost, AutomaticRecoveryEnabled = false };
+
+					if (exclusiveConnections)
+					{
+						conn = connFac.CreateConnection();
+						channel = conn.CreateModel();
+					}
+					else
+					{
+						if (conn == null)
+						{
+							conn = connFac.CreateConnection();
+							channel = conn.CreateModel();
+						}
+					}
+
+					var q = "q." + i;
+
+					tasks.Add(Task.Run(() => SendLegacyCalls(q, channel, howManyCalls)));
+				}
+			}
+			else
+			{
+				RabbitMqNext.IConnection conn = null;
+				RabbitMqNext.IChannel channel = null;
+
+				for (int i = 0; i < howManyQueues; i++)
+				{
+					if (exclusiveConnections)
+					{
+						conn = await RabbitMqNext.ConnectionFactory.Connect(host, vhost, user, pwd, recoverySettings: null);
+						channel = await conn.CreateChannel();
+					}
+					else
+					{
+						if (conn == null)
+						{
+							conn = await RabbitMqNext.ConnectionFactory.Connect(host, vhost, user, pwd, recoverySettings: null);
+							channel = await conn.CreateChannel();
+						}
+					}
+
+					var q = "q." + i;
+
+					tasks.Add(Task.Run(() => SendModernCalls(q, channel, howManyCalls)));
+				}
+			}
+
+
+			Console.WriteLine("Ready");
+
+			Thread.CurrentThread.Join();
+		}
+
+		private async void SendModernCalls(string queue, IChannel channel, int howManyCalls)
+		{
+			_startSync.Wait();
+
+			var helper = await channel.CreateRpcHelper(ConsumeMode.SingleThreaded, null);
+			var buffer = Encoding.UTF8.GetBytes("Some content not too big or too short I think");
+			var watch = new Stopwatch();
+
+			for (int i = 0; i < howManyCalls; i++)
+			{
+				watch.Restart();
+				await helper.Call("", queue, BasicProperties.Empty, new ArraySegment<byte>(buffer), false);
+				watch.Stop();
+			}
+		}
+
+		private void SendLegacyCalls(string queue, IModel channel, int howManyCalls)
+		{
+			_startSync.Wait();
+
+			var tempQueue = channel.QueueDeclare();
+
+			for (int i = 0; i < howManyCalls; i++)
+			{
+				
+			}
+		}
+
+	}
+}
---
+++ test/Perf/PerfTestMultQServer/PerfTestMultQServerProgram.cs
@@ -0,0 +1,162 @@
+﻿namespace PerfTestMultQServer
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Configuration;
+	using System.Threading;
+	using System.Threading.Tasks;
+	using RabbitMqNext;
+	using RabbitMQ.Client;
+	using RabbitMQ.Client.Events;
+
+	class PerfTestMultQServerProgram
+	{
+		static void Main(string[] args)
+		{
+			new PerfTestMultQServerProgram().Run(args).Wait();
+		}
+
+		private async Task Run(string[] args)
+		{
+			var host = ConfigurationManager.AppSettings["rabbit.host"];
+			var user = ConfigurationManager.AppSettings["rabbit.admuser"];
+			var pwd = ConfigurationManager.AppSettings["rabbit.admpwd"];
+			var vhost = ConfigurationManager.AppSettings["rabbit.vhost"];
+
+
+			int howManyQueues = 2;
+			bool exclusiveConnections = ConfigurationManager.AppSettings["exclusiveConnections"] == "true";
+			bool useOfficialClient = ConfigurationManager.AppSettings["useOfficialClient"] == "true";
+
+			if (useOfficialClient)
+			{
+				RabbitMQ.Client.IConnection conn = null;
+				RabbitMQ.Client.IModel channel = null;
+
+				for (int i = 0; i < howManyQueues; i++)
+				{
+					var connFac = new RabbitMQ.Client.ConnectionFactory() { HostName = host, UserName = user, Password = pwd, VirtualHost = vhost, AutomaticRecoveryEnabled = false };
+
+					if (exclusiveConnections)
+					{
+						conn = connFac.CreateConnection();
+						channel = conn.CreateModel();
+					}
+					else
+					{
+						if (conn == null)
+						{
+							conn = connFac.CreateConnection();
+							channel = conn.CreateModel();
+						}
+					}
+
+					var q = "q." + i;
+					channel.QueueDeclareNoWait(q, durable: true, exclusive: true, autoDelete: false, arguments: null);
+
+					channel.BasicConsume(q, false, "con_" + q, arguments: null, consumer: new Consumer(channel));
+				}
+			}
+			else
+			{
+				RabbitMqNext.IConnection conn = null;
+				RabbitMqNext.IChannel channel = null;
+
+				for (int i = 0; i < howManyQueues; i++)
+				{
+					if (exclusiveConnections)
+					{
+						conn = await RabbitMqNext.ConnectionFactory.Connect(host, vhost, user, pwd, recoverySettings: null);
+						channel = await conn.CreateChannel();
+					}
+					else
+					{
+						if (conn == null)
+						{
+							conn = await RabbitMqNext.ConnectionFactory.Connect(host, vhost, user, pwd, recoverySettings: null);
+							channel = await conn.CreateChannel();
+						}
+					}
+
+					var q = "q." + i;
+					
+					await channel.QueueDeclare(q, passive: false, durable: true, exclusive: true, autoDelete: false, arguments: null,
+						waitConfirmation: false);
+
+					await channel.BasicConsume(ConsumeMode.ParallelWithBufferCopy, BuildConsumerFn(channel), q, "consumer_" + q, 
+											   false, true, arguments: null, waitConfirmation: false);
+				}
+			}
+
+
+			Console.WriteLine("Ready");
+
+			Thread.CurrentThread.Join();
+		}
+
+		private Func<MessageDelivery, Task> BuildConsumerFn(IChannel channel)
+		{
+			return async delivery =>
+			{
+				var data = new byte[1024];
+				var read = 0;
+				while (read < delivery.bodySize)
+				{
+					read += delivery.stream.Read(data, read, delivery.bodySize - read);
+				}
+
+				var prop = channel.RentBasicProperties();
+				prop.CorrelationId = delivery.properties.CorrelationId;
+
+				channel.BasicPublishFast("", delivery.properties.ReplyTo, false, prop, new ArraySegment<byte>(data, 0, read));
+
+				channel.BasicAck(delivery.deliveryTag, false);
+			};
+		}
+	}
+
+	internal class Consumer : IBasicConsumer
+	{
+		private readonly IModel _channel;
+
+		public Consumer(IModel channel)
+		{
+			_channel = channel;
+		}
+
+		public void HandleBasicDeliver(string consumerTag, ulong deliveryTag, bool redelivered, string exchange, string routingKey,
+			IBasicProperties properties, byte[] body)
+		{
+			var data = new byte[body.Length];
+			// to be equal comparisson, need to copy buffer
+			Buffer.BlockCopy(body, 0, data, 0, body.Length);
+
+			var repProp = _channel.CreateBasicProperties();
+
+			repProp.CorrelationId = properties.CorrelationId;
+
+			_channel.BasicPublish("", properties.ReplyTo, false, repProp, data);
+
+			_channel.BasicAck(deliveryTag, false);
+		}
+
+		public void HandleBasicCancel(string consumerTag)
+		{
+		}
+
+		public void HandleBasicCancelOk(string consumerTag)
+		{
+		}
+
+		public void HandleBasicConsumeOk(string consumerTag)
+		{
+		}
+
+		public void HandleModelShutdown(object model, ShutdownEventArgs reason)
+		{
+		}
+
+		public IModel Model { get { return _channel; } }
+		public event EventHandler<ConsumerEventArgs> ConsumerCancelled;
+	}
+}
