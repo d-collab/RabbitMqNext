d18479ece2f2a6f29609ca356e9d836fb4132e32
hammett <hammett@gmail.com>
2016-07-19 17:45:05 -0700

    minor buffer copying refactoring

--- src/RabbitMqNext/Internals/BufferUtil.cs
+++ src/RabbitMqNext/Internals/BufferUtil.cs
@@ -31,24 +31,41 @@ namespace RabbitMqNext.Internals
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		static unsafe void CustomCopy(void* dest, void* src, int count)
+		private static unsafe void CustomCopy(void* dest, void* src, int count)
 		{
 			int block;
 
 			block = count >> 3;
+			{ 
+				ulong* pDest = (ulong*)dest;
+				ulong* pSrc = (ulong*)src;
 
-			long* pDest = (long*)dest;
-			long* pSrc = (long*)src;
-
-			for (int i = 0; i < block; i++)
-			{
-				*pDest = *pSrc; pDest++; pSrc++;
+				for (int i = 0; i < block; i++)
+				{
+					*pDest = *pSrc; pDest++; pSrc++;
+				}
+				dest = pDest;
+				src = pSrc;
+				count = count - (block << 3);
 			}
-			dest = pDest;
-			src = pSrc;
-			count = count - (block << 3);
 
-			if (count > 0)
+//			block = count >> 2;
+//			{
+//				uint* pDest = (uint*)dest;
+//				uint* pSrc = (uint*)src;
+//
+//				for (int i = 0; i < block; i++)
+//				{
+//					*pDest = *pSrc; pDest++; pSrc++;
+//				}
+//
+//				dest = pDest;
+//				src = pSrc;
+//
+//				count = count - (block << 2);
+//			}
+
+			if (count > 0) // remainder
 			{
 				byte* pDestB = (byte*)dest;
 				byte* pSrcB = (byte*)src;
